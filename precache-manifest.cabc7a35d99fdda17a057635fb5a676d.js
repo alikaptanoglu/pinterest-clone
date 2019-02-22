695c48846b01fb9e906f2aa961cc29761a097f3a
Abhaya Jeyaraman <abhaya0405@gmail.com>
2019-02-22 23:47:25 +0530

    Updates

---
+++ precache-manifest.cabc7a35d99fdda17a057635fb5a676d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dca167b9b6008ba193c9",
+    "url": "/pinterest-clone/static/js/runtime~main.dca167b9.js"
+  },
+  {
+    "revision": "cb4cfc32801f65458d50",
+    "url": "/pinterest-clone/static/js/main.cb4cfc32.chunk.js"
+  },
+  {
+    "revision": "bcc18b12c83e8eb635ba",
+    "url": "/pinterest-clone/static/js/2.bcc18b12.chunk.js"
+  },
+  {
+    "revision": "cb4cfc32801f65458d50",
+    "url": "/pinterest-clone/static/css/main.f6d92e94.chunk.css"
+  },
+  {
+    "revision": "0cb18c7036f038fde2958283f4e69c6f",
+    "url": "/pinterest-clone/index.html"
+  }
+];
